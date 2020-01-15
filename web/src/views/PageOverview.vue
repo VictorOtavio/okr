d99d58ffcd93e9d70c8f011dd7c6ab0af9af14d5
Victor Otávio Ferreira <victor@otavioferreira.com.br>
2020-01-15 10:57:57 -0300

    Well, it's doing something.

--- web/src/views/PageOverview.vue
+++ web/src/views/PageOverview.vue
@@ -1,5 +1,14 @@
 <template>
-  <div></div>
+  <section class="section">
+    <h1 class="title is-primary has-text-centered">4OKR</h1>
+    <p>
+      Lorem ipsum, dolor sit amet <a href="#">teste</a> consectetur adipisicing
+      elit. Nesciunt maiores natus consequuntur corrupti laborum iste animi.
+      Enim hic recusandae laboriosam? Earum nam neque atque porro nihil
+      repudiandae sapiente, blanditiis beatae?
+    </p>
+    <code>lorem ipsum</code>
+  </section>
 </template>
 
 <script>
