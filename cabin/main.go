bb848e54328495d62dda3e0a1493762e4b35c24d
Victor Otávio Ferreira <victor@otavioferreira.com.br>
2020-01-15 12:00:58 -0300

    Fix hard-coded [object Object] string (thanks!)

--- cabin/main.go
+++ cabin/main.go
@@ -11,18 +11,41 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", handler)
+
+	mux.HandleFunc("/quarters", quarters)
+	mux.HandleFunc("/objectives", objectives)
+	mux.HandleFunc("/key-results", keyResults)
+
 	err := http.ListenAndServe(
 		fmt.Sprintf(":%s", config.GetEnv("APP_PORT", "8000")),
 		handlers.CompressHandler(mux),
 	)
+
 	if err != nil {
 		log.Fatal("Erro ao iniciar o servidor HTTP: ", err)
 		return
 	}
 }
 
-func handler(w http.ResponseWriter, req *http.Request) {
+func quarters(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	if (*req).Method == "OPTIONS" {
+		return
+	}
+
+	fmt.Fprintln(w, "[]")
+}
+
+func objectives(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	if (*req).Method == "OPTIONS" {
+		return
+	}
+
+	fmt.Fprintln(w, "[]")
+}
+
+func keyResults(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	if (*req).Method == "OPTIONS" {
 		return
