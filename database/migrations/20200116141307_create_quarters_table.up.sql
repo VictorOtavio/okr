5980be9a8d36940a863dd48ba1486251c84e64a6
Victor Otávio Ferreira <victor@otavioferreira.com.br>
2020-01-23 15:00:39 -0300

    mudança total de paradigma

--- database/migrations/20200116141307_create_quarters_table.up.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS `quarters` (
-    `id` INTEGER PRIMARY KEY AUTOINCREMENT,
-    `title` TEXT NOT NULL,
-    `start_date` TEXT,
-    `end_date` TEXT
-);
--- database/migrations/20200116151600_create_objectives_table.up.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS `objectives` (
-    `id` INTEGER PRIMARY KEY AUTOINCREMENT,
-    `quarter_id` INTEGER NOT NULL,
-    `icon` TEXT NOT NULL,
-    `description` INTEGER NOT NULL
-);
